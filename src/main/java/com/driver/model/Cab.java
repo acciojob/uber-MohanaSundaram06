20f05e68c67debf58b9dd7b966f4bbe9e030e689
Mohana Sundaram K <mohanasundaramk06@gmail.com>
2023-06-10 20:57:47 +0530

    Implementation 1.0

---
+++ src/main/java/com/driver/model/Cab.java
@@ -0,0 +1,55 @@
+package com.driver.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cab")
+public class Cab {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cabId;
+
+    private int perKmRate;
+
+    private boolean availabe;
+
+    @JoinColumn
+    @OneToOne
+    private Driver driver;
+
+    public Cab() {
+    }
+
+    public int getCabId() {
+        return cabId;
+    }
+
+    public void setCabId(int cabId) {
+        this.cabId = cabId;
+    }
+
+    public int getPerKmRate() {
+        return perKmRate;
+    }
+
+    public void setPerKmRate(int perKmRate) {
+        this.perKmRate = perKmRate;
+    }
+
+    public boolean isAvailabe() {
+        return availabe;
+    }
+
+    public void setAvailabe(boolean availabe) {
+        this.availabe = availabe;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+}
---
+++ src/main/java/com/driver/model/Customer.java
@@ -0,0 +1,56 @@
+package com.driver.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int customerId;
+
+    private String password;
+
+    private String mobileNo;
+
+    @OneToMany(mappedBy = "customer",cascade = CascadeType.ALL)
+    private List<TripBooking> tripBookingList = new ArrayList<>();
+
+    public Customer() {
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public List<TripBooking> getTripBookingList() {
+        return tripBookingList;
+    }
+
+    public void setTripBookingList(List<TripBooking> tripBookingList) {
+        this.tripBookingList = tripBookingList;
+    }
+}
